de49587d1a8013ed46ff56dfefe7488f9acbd0a3
Ekaterina Vakhromeeva <vakhromeevae@gmail.com>
2018-08-06 14:39:15 +0300

    Layouts, data structure

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const header = ['Mark','Model','Type','Year'];
+
 const data = [
-  ['Mark', 'Ford'],
-  ['Model', 'Focus'],
-  ['Type', 'Hatchback'],
-  ['Year', 2014],
+  ["Ford", "Focus III", "Hatchback", 2014],
+  ["Ford", "Mondeo", "Sedan", 2009]
 ];
 
 class App extends Component {
@@ -17,29 +17,29 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">RBO Test App</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <h2> Здесь будет моя таблица </h2>
         <table className="Cars">
           <thead className="Cars-header">
             <tr>
-              <td>{data[0][0]}</td>
-              <td>{data[1][0]}</td>
-              <td>{data[2][0]}</td>
-              <td>{data[3][0]}</td>
+              <td>{header[0]}</td>
+              <td>{header[1]}</td>
+              <td>{header[2]}</td>
+              <td>{header[3]}</td>
             </tr>
           </thead>
           <tbody className="Cars-body">
-          { 
             <tr>
+              <td>{data[0][0]}</td>
               <td>{data[0][1]}</td>
-              <td>{data[1][1]}</td>
-              <td>{data[2][1]}</td>
-              <td>{data[3][1]}</td>
+              <td>{data[0][2]}</td>
+              <td>{data[0][3]}</td>
             </tr>
-          }
-          
+            <tr>
+              <td>{data[1][0]}</td>
+              <td>{data[1][1]}</td>
+              <td>{data[1][2]}</td>
+              <td>{data[1][3]}</td>
+            </tr>
           </tbody>
         </table>
       </div>
