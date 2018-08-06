f6c295f282f0a0974fae75603506ee71db7e1e39
Ekaterina Vakhromeeva <vakhromeevae@gmail.com>
2018-08-06 13:39:57 +0300

    static table

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {CSVDownload} from 'react-csv';
+
+const data = [
+  ['Alexander', 345345, 887423],
+  ['Paul', 2347, 76323],
+  ['Larisa', 745, 54234],
+];
 
 class App extends Component {
   render() {
@@ -14,6 +19,25 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2> Здесь будет моя таблица </h2>
+        <table className="Cars">
+          <thead className="Cars-header">
+            <tr>
+              <td>Mark</td>
+              <td>Model</td>
+              <td>Type</td>
+              <td>Year</td>
+            </tr>
+          </thead>
+          <tbody className="Cars-body">
+          <tr>
+              <td>Ford</td>
+              <td>F150</td>
+              <td>Truck</td>
+              <td>2018</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
