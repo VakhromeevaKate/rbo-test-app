523f1b09b122f3bab2af71c332bc1854dcee843b
Ekaterina Vakhromeeva <vakhromeevae@gmail.com>
2018-08-07 12:48:50 +0300

    Now we can search in all fields

--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -3,13 +3,12 @@ import ProductRow from './ProductRow';
 
 class ProductTable extends React.Component {
     render() {
-      var rows = [];
-      /*this.props.products.forEach(function(product) {
-        rows.push(<ProductRow product={product} key={product.name} />);
-      });*/
-      
+    var rows = [];
     this.props.products.forEach((product) => {
-      if (product.Mark.indexOf(this.props.filterText) === -1) {
+      if (product.Mark.toUpperCase().indexOf(this.props.filterText.toUpperCase()) === -1 &&
+          product.Model.toUpperCase().indexOf(this.props.filterText.toUpperCase()) === -1 &&
+          product.Type.toUpperCase().indexOf(this.props.filterText.toUpperCase()) === -1 &&
+          product.Year.toString().indexOf(this.props.filterText) === -1 ) {
         return;
       }
       rows.push(<ProductRow product={product} key={product.Id} />);
