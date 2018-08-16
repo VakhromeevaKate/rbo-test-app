503038b3cbaf86ea1f65813e73bf8ff39c901ba5
Ekaterina Vakhromeeva <vakhromeevae@gmail.com>
2018-08-16 15:50:18 +0300

    Now it really works correct. Ready for merging into master.

--- src/components/CommonProducts.js
+++ src/components/CommonProducts.js
@@ -1,19 +1,53 @@
 import React from 'react';
+import CommonProductRow from './CommonProductRow';
 
-class CommonProduct extends React.Component { 
-  render() {
-      return (
-        <tr>
-          <td>{this.props.product.Mark}</td>
-          <td>{this.props.product.Model}</td>
-          <td>{this.props.product.Type}</td>
-          <td>{this.props.product.Country}</td>
-          <td>{this.props.product.Color}</td>
-          <td>{this.props.product.Options}</td>
-          <td>{this.props.product.Year}</td>
-        </tr>
-      );
+class CommonProducts extends React.Component { 
+    render() {
+        let product;
+        let common = [];
+        if (this.props.productId !== null && this.props.productRowId){ 
+            product = this.props.products[this.props.productRowId];
+            console.log(product);
+            this.props.products.forEach((entity) => {
+                if ( entity.Id !== this.props.productId &&
+            
+                    ((entity.Color.toUpperCase() === product.Color.toUpperCase() &&
+                    entity.Type.toUpperCase() === product.Type.toUpperCase()) ||
+
+                    (entity.Options.toUpperCase() === product.Options.toUpperCase() &&
+                    entity.Type.toUpperCase() === product.Type.toUpperCase()) 
+                    || 
+                    (entity.Type.toUpperCase() === product.Type.toUpperCase() && 
+                    entity.Options.toUpperCase() === product.Options.toUpperCase() ))
+                ) {
+                    common.push(<CommonProductRow showDetails={this.handleShowDetails} product={entity} key={entity.Id} />);
+                }
+            });
+            console.log(common);
+        }
+        if (common.length > 0){
+            return (
+                <div>
+                <h2>You may also like: </h2>
+                <table className="Cars">
+                    <thead className="Cars-header">
+                    <tr>
+                        <th>Mark</th>
+                        <th>Model</th>
+                        <th>Type</th>
+                        <th>Country</th>
+                        <th>Color</th>
+                        <th>Options</th>
+                        <th>Year</th>
+                    </tr>
+                    </thead>
+                    <tbody className="Cars-body">{common}</tbody>
+                </table>
+            </div>
+            );
+        }
+        return null;
+      }
     }
-  }
-  
-export default CommonProduct;
+    
+  export default CommonProducts;
--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -4,22 +4,8 @@ import CommonProducts from './CommonProducts';
 class ProductDetails extends React.Component {
     render () {
         let product;
-        let common = [];
-        if (this.props.productId !== null && this.props.productRowId){ 
-            product = this.props.products[this.props.productRowId];
-            this.props.products.forEach((entity) => {
-                if ( entity.Id !== this.props.productId &&
-                    ((entity.Color.toUpperCase()  === product.Color.toUpperCase() &&
-                    entity.Type.toUpperCase() === product.Type.toUpperCase()) ||
-                    (entity.Options.toUpperCase().indexOf(product.Options.toUpperCase()) !== -1 &&
-                    entity.Type.toUpperCase() === product.Type.toUpperCase()) || 
-                    (entity.Type.toUpperCase() === product.Type.toUpperCase() && 
-                    entity.Options.toUpperCase().indexOf(product.Options.toUpperCase() !== -1)))) {
-                    common.push(<CommonProducts showDetails={this.handleShowDetails} product={entity} key={entity.Id} />);
-                }
-            });
-        }
-        
+        if (this.props.productId !== null){ 
+            product = this.props.products[this.props.productRowId];}
         return (
             <div onClick={() => this.props.showDetails(false)}>
                 <h1>Car details</h1>
@@ -45,33 +31,21 @@ class ProductDetails extends React.Component {
                             <td>{product.Country}</td>
                         </tr>
                         <tr>
+                            <td>Color</td>
+                            <td>{product.Color}</td>
+                        </tr>
+                        <tr>
                             <td>Options</td>
                             <td>{product.Options}</td>
                         </tr>
                     </tbody>
                 </table>
-                <div>
-                    <h2>You may also like: </h2>
-                    <table className="Cars">
-                        <thead className="Cars-header">
-                        <tr>
-                            <th>Mark</th>
-                            <th>Model</th>
-                            <th>Type</th>
-                            <th>Country</th>
-                            <th>Color</th>
-                            <th>Options</th>
-                            <th>Year</th>
-                        </tr>
-                        </thead>
-                        <tbody className="Cars-body">{common}</tbody>
-                    </table>
-                </div>
+                <CommonProducts 
+                    products={this.props.products} 
+                    productId={this.props.productId}
+                    productRowId={this.props.productRowId}/>
             </div>
         );
-    }
-    getProductDetails(productId){
-
     }
 }
 
