38f52ada877f0916bd7f3e369dbae40babb035a3
Ekaterina Vakhromeeva <vakhromeevae@gmail.com>
2018-08-16 13:34:31 +0300

    deleted

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import FilterableProductTable from './components/FilterableProductTable';
-import GetDataFromCSV from './utils/GetDataFromCSV';
-
-let data = new GetDataFromCSV();
+import Papa from 'papaparse';
+import autoDataset from './data-input/data-auto.csv';
+import attributesDataset from './data-input/data-attributes.csv';
+import colorsDataset from './data-input/data-colors.csv';
+import countriesDataset from './data-input/data-countries.csv';
+import optionsDataset from './data-input/data-options.csv';
+import $ from "jquery";
+import PrepareData from './utils/PrepareData';
 
 const PRODUCTS = [
   {Id: '1', Mark: 'Ford', Model: "Focus III", Type: "Hatchback", Year: 2014, Color: "Black", Options: "Cruise, LED", Country: "USA"},
@@ -17,6 +22,79 @@ const PRODUCTS = [
 ];
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      ready: false,
+      autos: [],
+      attributes: [],
+      colors: [],
+      countries: [],
+      options: []
+    };
+  }
+  componentDidMount() {
+    const config = {
+      delimiter: ',',
+      quoteChar: '"',
+      escapeChar: '"',
+      header: true,
+      trimHeaders: false
+    }  
+    let _this = this;
+    let counter = 0;
+    $.ajax({
+      url: autoDataset,
+      method: "get",
+      type: "text",
+      success:  function(data) {
+        _this.setState({autos: Papa.parse(data, config).data});
+        counter ++;
+        if (counter === 5) _this.setState({ready: true});
+      }
+    });
+    $.ajax({
+      url: attributesDataset,
+      method: "get",
+      type: "text",
+      success:  function(data) {
+        _this.setState({attributes: Papa.parse(data, config).data});
+        counter ++;
+        if (counter === 5) _this.setState({ready: true});
+      }
+    });
+    $.ajax({
+      url: colorsDataset,
+      method: "get",
+      type: "text",
+      success:  function(data) {
+        _this.setState({colors: Papa.parse(data, config).data});
+        counter ++;
+        if (counter === 5) _this.setState({ready: true});
+      }
+    });
+    $.ajax({
+      url: countriesDataset,
+      method: "get",
+      type: "text",
+      success:  function(data) {
+        _this.setState({countries: Papa.parse(data, config).data});
+        counter ++;
+        if (counter === 5) _this.setState({ready: true});
+      }
+    });
+    $.ajax({
+      url: optionsDataset,
+      method: "get",
+      type: "text",
+      success:  function(data) {
+        _this.setState({options: Papa.parse(data, config).data});
+        counter ++;
+        if (counter === 5) _this.setState({ready: true});
+      }
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -25,7 +103,9 @@ class App extends Component {
           <h1 className="App-title">RBO Test App</h1>
         </header>
         <h3>Searching for a car?</h3>
-        <FilterableProductTable products={PRODUCTS}/>
+        <FilterableProductTable 
+        products={PrepareData(this.state.autos, this.state.attributes, this.state.colors, this.state.countries, this.state.options)} 
+        ready={this.state.ready} />
       </div>
     );
   }
