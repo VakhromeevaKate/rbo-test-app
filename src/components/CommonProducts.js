f678faded06b9c4d20bdb5e0a2c2c3820d33f912
Ekaterina Vakhromeeva <vakhromeevae@gmail.com>
2018-08-24 16:43:20 +0300

    CommonProducts - render dividing

--- src/components/CommonProducts.js
+++ src/components/CommonProducts.js
@@ -2,20 +2,17 @@ import React from 'react';
 import CommonProductRow from './CommonProductRow';
 
 class CommonProducts extends React.Component { 
-    render() {
+    renderCommon() {
         let product;
         let common = [];
         if (this.props.productId !== null && this.props.productRowId){ 
             product = this.props.products[this.props.productRowId];
             this.props.products.forEach((entity) => {
                 if ( entity.Id !== this.props.productId &&
-            
                     ((entity.Color.toUpperCase() === product.Color.toUpperCase() &&
                     entity.Type.toUpperCase() === product.Type.toUpperCase()) ||
-
                     (entity.Options.toUpperCase() === product.Options.toUpperCase() &&
-                    entity.Type.toUpperCase() === product.Type.toUpperCase()) 
-                    || 
+                    entity.Type.toUpperCase() === product.Type.toUpperCase()) || 
                     (entity.Type.toUpperCase() === product.Type.toUpperCase() && 
                     entity.Options.toUpperCase() === product.Options.toUpperCase() ))
                 ) {
@@ -25,26 +22,30 @@ class CommonProducts extends React.Component {
         }
         if (common.length > 0){
             return (
-                <div>
-                <h2>You may also like: </h2>
-                <table className="Cars">
-                    <thead className="Cars-header">
-                    <tr>
-                        <th>Mark</th>
-                        <th>Model</th>
-                        <th>Type</th>
-                        <th>Country</th>
-                        <th>Color</th>
-                        <th>Options</th>
-                        <th>Year</th>
-                    </tr>
-                    </thead>
-                    <tbody className="Cars-body">{common}</tbody>
-                </table>
-            </div>
-            );
-        }
-        return null;
+                <tbody className="Cars-body">{common}</tbody>
+            )
+        } return null;
+    }
+    render() {
+        return (
+            <div>
+            <h2>You may also like: </h2>
+            <table className="Cars">
+                <thead className="Cars-header">
+                <tr>
+                    <th>Mark</th>
+                    <th>Model</th>
+                    <th>Type</th>
+                    <th>Country</th>
+                    <th>Color</th>
+                    <th>Options</th>
+                    <th>Year</th>
+                </tr>
+                </thead>
+                {this.renderCommon()}
+            </table>
+        </div>
+        );
       }
     }
     
