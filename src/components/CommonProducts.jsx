b99db5e678a0a83947c5a7395e7fff23c6e3f193
Ekaterina Vakhromeeva <vakhromeevae@gmail.com>
2018-09-10 16:26:54 +0300

    CommonProducts becomes a functional component

--- src/components/CommonProducts.jsx
+++ src/components/CommonProducts.jsx
@@ -1,60 +1,58 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import CommonProductRow from './CommonProductRow';
 
-class CommonProducts extends PureComponent {
-    render() {
-        return (
-            <div>
-                <h2>You may also like: </h2>
-                <table className="Cars">
-                    <thead className="Cars-header">
-                        <tr>
-                            <th>Mark</th>
-                            <th>Model</th>
-                            <th>Type</th>
-                            <th>Country</th>
-                            <th>Color</th>
-                            <th>Options</th>
-                            <th>Year</th>
-                        </tr>
-                    </thead>
-                    {this.renderCommon()}
-                </table>
-            </div>
-        );
-    }
+const CommonProducts = (props) => {
+    return (
+        <div>
+            <h2>You may also like: </h2>
+            <table className="Cars">
+                <thead className="Cars-header">
+                    <tr>
+                        <th>Mark</th>
+                        <th>Model</th>
+                        <th>Type</th>
+                        <th>Country</th>
+                        <th>Color</th>
+                        <th>Options</th>
+                        <th>Year</th>
+                    </tr>
+                </thead>
+                {renderCommon(props)}
+            </table>
+        </div>
+    );
+}
 
-    renderCommon() {
-        let product;
-        let common = [];
-        if (this.props.productId !== null && this.props.productRowId) {
-            product = this.props.products[this.props.productRowId];
-            this.props.products.forEach((entity) => {
-                if (entity.Id !== this.props.productId &&
-                    ((entity.Color.toUpperCase() === product.Color.toUpperCase() &&
+function renderCommon(props) {
+    let product;
+    let common = [];
+    if (props.productId !== null && props.productRowId) {
+        product = props.products[props.productRowId];
+        props.products.forEach((entity) => {
+            if (entity.Id !== props.productId &&
+                ((entity.Color.toUpperCase() === product.Color.toUpperCase() &&
+                    entity.Type.toUpperCase() === product.Type.toUpperCase()) ||
+                    (entity.Options.toUpperCase() === product.Options.toUpperCase() &&
                         entity.Type.toUpperCase() === product.Type.toUpperCase()) ||
-                        (entity.Options.toUpperCase() === product.Options.toUpperCase() &&
-                            entity.Type.toUpperCase() === product.Type.toUpperCase()) ||
-                        (entity.Type.toUpperCase() === product.Type.toUpperCase() &&
-                            entity.Options.toUpperCase() === product.Options.toUpperCase()))
-                ) {
-                    common.push(entity);
-                }
-            });
-        }
-        if (common.length > 0) {
-            return (
-                <tbody className="Cars-body">{common.map((entity) => {
-                    return (
-                        <CommonProductRow
-                            showDetails={this.handleShowDetails}
-                            product={entity}
-                            key={entity.Id}
-                        />
-                    )
-                })}</tbody>
-            )
-        } return null;
+                    (entity.Type.toUpperCase() === product.Type.toUpperCase() &&
+                        entity.Options.toUpperCase() === product.Options.toUpperCase()))
+            ) {
+                common.push(entity);
+            }
+        });
     }
+    if (common.length > 0) {
+        return (
+            <tbody className="Cars-body">{common.map((entity) => {
+                return (
+                    <CommonProductRow
+                        showDetails={true}
+                        product={entity}
+                        key={entity.Id}
+                    />
+                )
+            })}</tbody>
+        )
+    } return null;
 }
 export default CommonProducts;
